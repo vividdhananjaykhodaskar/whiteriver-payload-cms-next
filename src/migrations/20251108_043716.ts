18cae5062d73cf846e39671db68b97eb6b0fd788
Dhananjay <vivid.dhananjaykhodaskar@gmail.com>
2025-11-08 11:50:01 +0530

    changes

--- src/migrations/20251108_043716.ts
+++ src/migrations/20251108_043716.ts
@@ -1,19 +1,6 @@
 import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-d1-sqlite'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
-  await db.run(sql`CREATE TABLE \`footer_links\` (
-  	\`id\` integer PRIMARY KEY NOT NULL,
-  	\`title\` text NOT NULL,
-  	\`order\` numeric NOT NULL,
-  	\`is_active\` integer DEFAULT true,
-  	\`url\` text NOT NULL,
-  	\`updated_at\` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) NOT NULL,
-  	\`created_at\` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) NOT NULL
-  );
-  `)
-  await db.run(sql`CREATE INDEX \`footer_links_updated_at_idx\` ON \`footer_links\` (\`updated_at\`);`)
-  await db.run(sql`CREATE INDEX \`footer_links_created_at_idx\` ON \`footer_links\` (\`created_at\`);`)
-  await db.run(sql`DROP TABLE \`footer_site_links\`;`)
   await db.run(sql`PRAGMA foreign_keys=OFF;`)
   await db.run(sql`CREATE TABLE \`__new_payload_locked_documents_rels\` (
   	\`id\` integer PRIMARY KEY NOT NULL,
@@ -27,7 +14,6 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	\`services_id\` integer,
   	\`sister_sites_id\` integer,
   	\`book_store_id\` integer,
-  	\`footer_links_id\` integer,
   	FOREIGN KEY (\`parent_id\`) REFERENCES \`payload_locked_documents\`(\`id\`) ON UPDATE no action ON DELETE cascade,
   	FOREIGN KEY (\`users_id\`) REFERENCES \`users\`(\`id\`) ON UPDATE no action ON DELETE cascade,
   	FOREIGN KEY (\`media_id\`) REFERENCES \`media\`(\`id\`) ON UPDATE no action ON DELETE cascade,
@@ -36,10 +22,9 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	FOREIGN KEY (\`services_id\`) REFERENCES \`services\`(\`id\`) ON UPDATE no action ON DELETE cascade,
   	FOREIGN KEY (\`sister_sites_id\`) REFERENCES \`sister_sites\`(\`id\`) ON UPDATE no action ON DELETE cascade,
   	FOREIGN KEY (\`book_store_id\`) REFERENCES \`book_store\`(\`id\`) ON UPDATE no action ON DELETE cascade,
-  	FOREIGN KEY (\`footer_links_id\`) REFERENCES \`footer_links\`(\`id\`) ON UPDATE no action ON DELETE cascade
   );
   `)
-  await db.run(sql`INSERT INTO \`__new_payload_locked_documents_rels\`("id", "order", "parent_id", "path", "users_id", "media_id", "magazines_id", "navigation_id", "services_id", "sister_sites_id", "book_store_id", "footer_links_id") SELECT "id", "order", "parent_id", "path", "users_id", "media_id", "magazines_id", "navigation_id", "services_id", "sister_sites_id", "book_store_id", "footer_links_id" FROM \`payload_locked_documents_rels\`;`)
+  await db.run(sql`INSERT INTO \`__new_payload_locked_documents_rels\`("id", "order", "parent_id", "path", "users_id", "media_id", "magazines_id", "navigation_id", "services_id", "sister_sites_id", "book_store_id") SELECT "id", "order", "parent_id", "path", "users_id", "media_id", "magazines_id", "navigation_id", "services_id", "sister_sites_id", "book_store_id" FROM \`payload_locked_documents_rels\`;`)
   await db.run(sql`DROP TABLE \`payload_locked_documents_rels\`;`)
   await db.run(sql`ALTER TABLE \`__new_payload_locked_documents_rels\` RENAME TO \`payload_locked_documents_rels\`;`)
   await db.run(sql`PRAGMA foreign_keys=ON;`)
@@ -53,7 +38,6 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_services_id_idx\` ON \`payload_locked_documents_rels\` (\`services_id\`);`)
   await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_sister_sites_id_idx\` ON \`payload_locked_documents_rels\` (\`sister_sites_id\`);`)
   await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_book_store_id_idx\` ON \`payload_locked_documents_rels\` (\`book_store_id\`);`)
-  await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_footer_links_id_idx\` ON \`payload_locked_documents_rels\` (\`footer_links_id\`);`)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
@@ -69,7 +53,6 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   `)
   await db.run(sql`CREATE INDEX \`footer_site_links_updated_at_idx\` ON \`footer_site_links\` (\`updated_at\`);`)
   await db.run(sql`CREATE INDEX \`footer_site_links_created_at_idx\` ON \`footer_site_links\` (\`created_at\`);`)
-  await db.run(sql`DROP TABLE \`footer_links\`;`)
   await db.run(sql`PRAGMA foreign_keys=OFF;`)
   await db.run(sql`CREATE TABLE \`__new_payload_locked_documents_rels\` (
   	\`id\` integer PRIMARY KEY NOT NULL,
--- src/migrations/20251108_052950.ts
+++ src/migrations/20251108_052950.ts
@@ -11,10 +11,6 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	\`created_at\` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) NOT NULL
   );
   `)
-  await db.run(sql`CREATE INDEX \`footer_link_updated_at_idx\` ON \`footer_link\` (\`updated_at\`);`)
-  await db.run(sql`CREATE INDEX \`footer_link_created_at_idx\` ON \`footer_link\` (\`created_at\`);`)
-  await db.run(sql`DROP TABLE \`footer_links\`;`)
-  await db.run(sql`PRAGMA foreign_keys=OFF;`)
   await db.run(sql`CREATE TABLE \`__new_payload_locked_documents_rels\` (
   	\`id\` integer PRIMARY KEY NOT NULL,
   	\`order\` integer,
@@ -57,20 +53,6 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
-  await db.run(sql`CREATE TABLE \`footer_links\` (
-  	\`id\` integer PRIMARY KEY NOT NULL,
-  	\`title\` text NOT NULL,
-  	\`order\` numeric NOT NULL,
-  	\`is_active\` integer DEFAULT true,
-  	\`url\` text NOT NULL,
-  	\`updated_at\` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) NOT NULL,
-  	\`created_at\` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) NOT NULL
-  );
-  `)
-  await db.run(sql`CREATE INDEX \`footer_links_updated_at_idx\` ON \`footer_links\` (\`updated_at\`);`)
-  await db.run(sql`CREATE INDEX \`footer_links_created_at_idx\` ON \`footer_links\` (\`created_at\`);`)
-  await db.run(sql`DROP TABLE \`footer_link\`;`)
-  await db.run(sql`PRAGMA foreign_keys=OFF;`)
   await db.run(sql`CREATE TABLE \`__new_payload_locked_documents_rels\` (
   	\`id\` integer PRIMARY KEY NOT NULL,
   	\`order\` integer,
@@ -83,7 +65,6 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   	\`services_id\` integer,
   	\`sister_sites_id\` integer,
   	\`book_store_id\` integer,
-  	\`footer_links_id\` integer,
   	FOREIGN KEY (\`parent_id\`) REFERENCES \`payload_locked_documents\`(\`id\`) ON UPDATE no action ON DELETE cascade,
   	FOREIGN KEY (\`users_id\`) REFERENCES \`users\`(\`id\`) ON UPDATE no action ON DELETE cascade,
   	FOREIGN KEY (\`media_id\`) REFERENCES \`media\`(\`id\`) ON UPDATE no action ON DELETE cascade,
@@ -92,10 +73,9 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   	FOREIGN KEY (\`services_id\`) REFERENCES \`services\`(\`id\`) ON UPDATE no action ON DELETE cascade,
   	FOREIGN KEY (\`sister_sites_id\`) REFERENCES \`sister_sites\`(\`id\`) ON UPDATE no action ON DELETE cascade,
   	FOREIGN KEY (\`book_store_id\`) REFERENCES \`book_store\`(\`id\`) ON UPDATE no action ON DELETE cascade,
-  	FOREIGN KEY (\`footer_links_id\`) REFERENCES \`footer_links\`(\`id\`) ON UPDATE no action ON DELETE cascade
   );
   `)
-  await db.run(sql`INSERT INTO \`__new_payload_locked_documents_rels\`("id", "order", "parent_id", "path", "users_id", "media_id", "magazines_id", "navigation_id", "services_id", "sister_sites_id", "book_store_id", "footer_links_id") SELECT "id", "order", "parent_id", "path", "users_id", "media_id", "magazines_id", "navigation_id", "services_id", "sister_sites_id", "book_store_id", "footer_links_id" FROM \`payload_locked_documents_rels\`;`)
+  await db.run(sql`INSERT INTO \`__new_payload_locked_documents_rels\`("id", "order", "parent_id", "path", "users_id", "media_id", "magazines_id", "navigation_id", "services_id", "sister_sites_id", "book_store_id") SELECT "id", "order", "parent_id", "path", "users_id", "media_id", "magazines_id", "navigation_id", "services_id", "sister_sites_id", "book_store_id" FROM \`payload_locked_documents_rels\`;`)
   await db.run(sql`DROP TABLE \`payload_locked_documents_rels\`;`)
   await db.run(sql`ALTER TABLE \`__new_payload_locked_documents_rels\` RENAME TO \`payload_locked_documents_rels\`;`)
   await db.run(sql`PRAGMA foreign_keys=ON;`)
@@ -109,5 +89,4 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_services_id_idx\` ON \`payload_locked_documents_rels\` (\`services_id\`);`)
   await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_sister_sites_id_idx\` ON \`payload_locked_documents_rels\` (\`sister_sites_id\`);`)
   await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_book_store_id_idx\` ON \`payload_locked_documents_rels\` (\`book_store_id\`);`)
-  await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_footer_links_id_idx\` ON \`payload_locked_documents_rels\` (\`footer_links_id\`);`)
 }
